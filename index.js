495201c2d9bd185031d4bdfaf20de4692d8b8001
Favixx <bogdandom52@gmail.com>
2023-10-11 17:55:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.use(
     origin: "*",
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 
 app.use("/uploads/images", express.static("uploads/images"));
