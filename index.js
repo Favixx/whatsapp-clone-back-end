1bf14ebf987f9eabd218e7d6c118089279bf405d
Favixx <bogdandom52@gmail.com>
2023-10-12 14:49:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,10 +29,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000/",
-      "https://whatsapp-clone-front-end-zeta.vercel.app/",
-    ],
+    origin: "https://whatsapp-clone-front-end-zeta.vercel.app/",
   },
 });
 
